eff67121f26678421951ef7387804793315a1e13
muxiaorui <muxiaorui@lvmama.com>
2018-05-21 14:06:59 +0800

    新增activityMQ test

--- src/main/java/com/example/passportdemo/PassportdemoApplication.java
+++ src/main/java/com/example/passportdemo/PassportdemoApplication.java
@@ -1,14 +1,23 @@
 package com.example.passportdemo;
 
+
+import org.apache.activemq.command.ActiveMQQueue;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import javax.jms.Queue;
 
 
 @SpringBootApplication
 @MapperScan("com.example.passportdemo.mapper")
 public class PassportdemoApplication {
+//	@Bean
+//	public Queue queue() {
+//		return new ActiveMQQueue("passport.queue");
+//	}
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext ctx=SpringApplication.run(PassportdemoApplication.class, args);
--- src/test/java/com/example/passportdemo/PassportdemoApplicationTests.java
+++ src/test/java/com/example/passportdemo/PassportdemoApplicationTests.java
@@ -1,16 +1,31 @@
 package com.example.passportdemo;
 
+import com.example.passportdemo.jms.Producer;
+import org.apache.activemq.command.ActiveMQQueue;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.jms.Destination;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PassportdemoApplicationTests {
 
+
+	@Autowired
+	private Producer producer;
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws Exception {
+		Destination destination = new ActiveMQQueue("passport.queue");
+
+		for(int i=0; i<100; i++){
+			producer.send(destination, "发送"+(i+1)+"消息："+"activityMQ");
+			System.out.println("发送"+(i+1)+"消息："+"activityMQ");
+			Thread.sleep(300);
+		}
 	}
 
 }
